95b1a65ae8653c0e8e56e4ccf61e8b05c73983db
Elin Rowlands <elinrow@gmail.com>
2026-01-23 13:39:32 +0000

    Update InstantiateCohorts

--- Study/Cohorts/InstantiateCohorts.R
+++ Study/Cohorts/InstantiateCohorts.R
@@ -1,22 +1,52 @@
-# get cardiovascular drugs
+### get acute MI
 
-card_drugs_cl <- importCodelist(here("Cohorts", "drugs"), type = "csv")
+acute_mi_cl <- CodelistGenerator::codesFromCohort(
+  path = here::here("Cohorts", "outcomes"),
+  cdm = cdm
+)
+
+cdm$acute_mi <- conceptCohort(
+  cdm = cdm,
+  conceptSet = acute_mi_cl,
+  name = "acute_mi"
+)
+
+
+# get beta blockers
+
+drugs_cl <- importCodelist(here("Cohorts", "drugs"), type = "csv")
+
 
 cdm <- generateDrugUtilisationCohortSet(
-  cdm = cdm, 
-  name = "cardiovascular_drugs",
-  conceptSet = card_drugs_cl,
+  cdm = cdm,
+  name = "cardio_drugs",
+  conceptSet = drugs_cl,
   gapEra = 7
 )
 
-cdm$cardiovascular_drugs <- cdm$cardiovascular_drugs |>
-  requirePriorDrugWashout(days = 30) |>
-  requireInDateRange(study_period)
+cdm$cardio_drugs <- cdm$cardio_drugs |>
+  requireIsFirstDrugEntry() |>
+  requireTableIntersect(
+    tableName = "acute_mi",
+    window = c(-30, 0),
+    intersections = c(1,Inf)
+  ) |>
+  requireInDateRange(study_period) |>
+  requireAge(ageRange = c(18,150)) |>
+  requirePriorObservation(minPriorObservation = 365)
 
-# identify cohorts that meet the min_cell_count requirements
-drug_count <- cohortCount(cdm$cardiovascular_drugs) |>
-  filter(number_subjects > min_cell_count)
 
-# restrict to these cohorts
-cdm$cardiovascular_drugs |>
+drug_count <- cohortCount(cdm$cardio_drugs) |>
+  filter(number_subjects >= min_cell_count)
+
+cdm$cardio_drugs |>
   subsetCohorts(cohortId = drug_count$cohort_definition_id)
+
+### Cohort Code Use
+
+if(isTRUE(run_code_use)){
+
+# code use here
+  
+}
+
