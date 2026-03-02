00ef875b61267ba82a6a82462dd55f3f2d40c76d
Ed Burn <9583964+edward-burn@users.noreply.github.com>
2026-03-02 13:42:14 +0000

    update phenotyper

--- diagnostics_shiny/server.R
+++ diagnostics_shiny/server.R
@@ -2525,18 +2525,20 @@ server <- function(input, output, session) {
 
   # log file -----
   output$summarise_log_file_gt <- gt::render_gt({
-    dataFiltered$summarise_log_file |>
+    
+    time_taken <- dataFiltered$summarise_log_file |>
       omopgenerics::tidy() |>
       dplyr::mutate(log_id = as.integer(log_id)) |>
       dplyr::arrange("cdm_name", "log_id")  |>
-      dplyr::select(cdm_name, variable_name,
+      dplyr::select(log_id,
+                    cdm_name, variable_name,
                     elapsed_time) |>
       dplyr::mutate(elapsed_time =
                       dplyr::if_else(!is.na(elapsed_time),
                                      sprintf("%d hours, %d minutes, %d seconds",
-                                             lubridate::hour(elapsed_time),
-                                             lubridate::minute(elapsed_time),
-                                             lubridate::second(elapsed_time)),
+                                             elapsed_time %/% 3600,   
+                                             (elapsed_time %% 3600) %/% 60, 
+                                             elapsed_time %% 60),
                                      NA_character_)) |>
       dplyr::mutate(elapsed_time = stringr::str_replace(
         elapsed_time, "0 hours, 0 minutes, ", "")) |>
@@ -2551,14 +2553,16 @@ server <- function(input, output, session) {
       dplyr::rename("task" = "variable_name",
                     estimate_value = "elapsed_time") |>
       dplyr::mutate(estimate_type = "character",
-                    estimate_name = "Time taken") |>
-      visOmopResults::visTable(
-        header = c("cdm_name", "estimate_name"),
-        rename = c("Database name" = "cdm_name"),
-        hide = "estimate_type") |>
+                    estimate_name = "Time taken")
+    
+    time_taken |> 
+      filter(!is.na(estimate_value)) |> 
+      visOmopResults::visTable(groupColumn = "cdm_name",
+                               rename = c("Database name" = "cdm_name"),
+                               hide = c("estimate_type", "estimate_name", "log_id")) |>
       gt::tab_options(container.width = "100%")
   })
-
+  
   # expectations ----
   createExpectationsOutput <- function(trigger_input, output_id) {
     filteredExpectations <- eventReactive(trigger_input(), {
